52b6d56d3a498c051ee2208705694aa4b97ef100
Sarah Vessels <cheshire137@gmail.com>
2017-04-15 09:47:10 -0500

    Add Slack token to users table

--- db/migrate/20170415010554_create_users.rb
+++ db/migrate/20170415010554_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :user_name
+      t.string :email, null: false
+      t.string :user_name, null: false
       t.string :spotify_access_token
       t.string :spotify_refresh_token
+      t.string :slack_access_token
       t.timestamps null: false
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,13 @@ ActiveRecord::Schema.define(version: 20170415010554) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "user_name"
+    t.string   "email",                 null: false
+    t.string   "user_name",             null: false
     t.string   "spotify_access_token"
     t.string   "spotify_refresh_token"
+    t.string   "slack_user_id"
+    t.string   "slack_user_name"
+    t.string   "slack_access_token"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
