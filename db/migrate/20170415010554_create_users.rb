b4dcd441b0e9c62f92e1b68fb1759fec950d8096
Sarah Vessels <cheshire137@gmail.com>
2017-04-14 20:08:57 -0500

    Configure database

---
+++ db/migrate/20170415010554_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :spotify_access_token
+      t.string :spotify_refresh_token
+      t.timestamps null: false
+    end
+  end
+end
